92754199504435d0452e7b01c996608158ce3729
Aditya Silwal <silwala@sheridancollege.ca>
2024-03-30 03:45:22 -0400

    observable method completed

--- src/app/pet-details/pet-details.component.ts
+++ src/app/pet-details/pet-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Pet} from "../pet";
+import {Subscription} from "rxjs";
+import {PetDataService} from "../pet-data.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-pet-details',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class PetDetailsComponent {
 
+  pet: Pet | undefined;
+  private petSub : Subscription | undefined;
+
+  constructor(petDataService : PetDataService, activatedRoute: ActivatedRoute) {
+
+    const idStr: string | null = activatedRoute.snapshot.paramMap.get('id');
+    const id: number | null = idStr !== null ? parseInt(idStr) : null;
+    if(id !== null){
+      this.petSub =
+        petDataService.getPetById(id).subscribe(pet => this.pet = pet);
+    }
+  }
+
+  ngOnDestroy(){
+    this.petSub?.unsubscribe();
+  }
 }
